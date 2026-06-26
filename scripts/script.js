678e899b16ffbc290f9245815d7596ca1fcae177
Treloso <diogo.trelosocnt@gmail.com>
2026-06-26 11:21:23 -0300

    feat(call-form): implement FormCallItem constructor for ticket creation

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,17 @@ const fullDesc = callForm.querySelector('#full-descript');
 const formDueDay = callForm.querySelector('#day');
 const formDueMonth = callForm.querySelector('#month');
 const formDueYear = callForm.querySelector('#year');
+
+// Construtor que cria um objeto de um Item de Chamado
+// Os parâmetros informados são os valores de cada entrada no elemento
+function FormCallItem(name, smallDesc, fullDesc, dueDay, dueMonth, dueYear) {
+    //utiliza randomUUID para gerar um identificador exclusivo
+    this.id = crypto.randomUUID();
+    
+    this.name = name;
+    this.smallDesc = smallDesc;
+    this.fullDesc = fullDesc;
+    this.dueDay = dueDay;
+    this.dueMonth = dueMonth;
+    this.dueYear = dueYear;
+}
