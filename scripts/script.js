3d67b94887b345b95d1d182e602071564fe8a325
Treloso <diogo.trelosocnt@gmail.com>
2026-06-26 10:50:21 -0300

    chore(call-form): cache DOM element references for form inputs

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const callForm = document.getElementById('call-form');
+const formName = callForm.querySelector('#name');
+const smallDesc = callForm.querySelector('#descript');
+const fullDesc = callForm.querySelector('#full-descript');
+const formDueDay = callForm.querySelector('#day');
+const formDueMonth = callForm.querySelector('#month');
+const formDueYear = callForm.querySelector('#year');
