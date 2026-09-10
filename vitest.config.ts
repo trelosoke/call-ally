c0f5bd30a126cbc28f96524cea269ba4d2c53eb5
Treloso <diogo.trelosocnt@gmail.com>
2026-09-10 17:53:05 -0300

    chore(test): move unit test setup into the vitest config
    - vite.config.js: remove the test block now superseded by vitest.config.ts
    - vitest.config.ts: wire vitest-setup.ts into the unit project

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
                     name: 'unit',
                     environment: 'node',
                     include: ['src/**/*.test.ts'],
+                    setupFiles: ['./vitest-setup.ts'],
                 }
             },
             {
